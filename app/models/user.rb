1e5f3d627f08220a411464f9de77c36c52a2aa48
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-04-07 14:50:02 +1000

    rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :topics
   has_many :posts
+
+  after_create :assign_default_role
+
+
+  def assign_default_role
+    self.add_role(:member) if self.roles.blank?
+  end
 end
